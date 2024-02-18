1dfee4cc8513b4da057c08ff3716bfbacc496771
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-02-18 08:44:03 +0800

    Updated index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mimon</title>
+    <title>Mimon Elias Maiquez</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -178,7 +178,7 @@
         <footer>
             insert footer
         </footer>
-        
+
     </main>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
