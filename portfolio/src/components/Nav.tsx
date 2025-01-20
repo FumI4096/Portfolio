c6c629d2a94cabe3cb130a68eabd881170163c1c
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-01-20 19:08:33 +0800

    Add Nav.tsx

---
+++ portfolio/src/components/Nav.tsx
@@ -0,0 +1,16 @@
+export default function Nav(){
+    return(
+        <nav>
+            <ul>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+            </ul>
+        </nav>
+    )
+}
+
