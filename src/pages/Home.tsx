8ce90a53ef3cef9ac35695f02ba1ae75826fe4f8
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-01-21 21:54:20 +0800

    Change padding value

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home(){
     return(
-        <header className="h-1000 flex bg-primary-gray-2 p-10rem">
+        <header className="h-1000 flex bg-primary-gray-2 p-14rem">
             <section>
                 <h1>Hello I am Seimon Elias S. Maiquez</h1>
             </section>
