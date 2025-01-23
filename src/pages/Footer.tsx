ebdf391c06576314570a8cb79542ca80a978df1b
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-01-23 21:04:19 +0800

    Import logo and add basic information

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,31 +1,44 @@
+import Icon from '../assets/images/icon.png';
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer(){
     return(
         <footer className="h-1000 flex flex-col bg-primary-gray-2 p-14rem justify-center">
             <section className="flex justify-between">
-                <article className="flex flex-row basis-full h-72">
-                    <h1>Mimon.</h1>
-                    <p></p>
+                <article className="flex flex-col basis-full h-72">
+                    <div className='mb-12'>
+                        <img src={Icon} alt="" className='h-10'/>
+                    </div>
+
+                    <p className='font-light text-white'> I’m a frontend developer based in the Philippines, from the province of Laguna. I have joined technology related curricular activities that emphasize ideas for application development, nurturing myself for hardware and software applications.
+                    </p>
                 </article>
-                <article className="flex basis-full justify-end h-72 gap-16">
-                    <a href="https://www.facebook.com/mimon.maiquez/" rel="noopener" target="_blank" className="w-auto h-auto">
-                        <FaFacebook className="text-4xl text-white cursor-pointer" />
-                    </a>
-                    <a href="https://github.com/FumI4096" rel="noopener" target="_blank" className="w-auto h-auto">
-                        <FaGithub className="text-4xl text-white cursor-pointer" />
-                    </a>
-                    <a href="https://www.instagram.com/se1_mon/" rel="noopener" target="_blank" className="w-auto h-auto">
-                        <FaInstagram className="text-4xl text-white cursor-pointer" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/seimon-elias-maiquez-3159602b1/" rel="noopener" target="_blank" className="w-auto h-auto">
-                        <FaLinkedin className="text-4xl text-white cursor-pointer" />
-                    </a>
-                </article>
+                <ul className="flex basis-full justify-end h-72 gap-16 list-none">
+                    <li className='w-auto h-auto'>
+                        <a href="https://www.facebook.com/mimon.maiquez/" rel="noopener" target="_blank" className="w-auto h-auto">
+                            <FaFacebook className="text-4xl text-white cursor-pointer" />
+                        </a>
+                    </li>
+                    <li className='w-auto h-auto'>
+                        <a href="https://github.com/FumI4096" rel="noopener" target="_blank" className="w-auto h-auto">
+                            <FaGithub className="text-4xl text-white cursor-pointer" />
+                        </a>
+                    </li>
+                    <li className='w-auto h-auto'>
+                        <a href="https://www.instagram.com/se1_mon/" rel="noopener" target="_blank" className="w-auto h-auto">
+                            <FaInstagram className="text-4xl text-white cursor-pointer" />
+                        </a>
+                    </li>
+                    <li className='w-auto h-auto'>
+                        <a href="https://www.linkedin.com/in/seimon-elias-maiquez-3159602b1/" rel="noopener" target="_blank" className="w-auto h-auto">
+                            <FaLinkedin className="text-4xl text-white cursor-pointer" />
+                        </a>
+                    </li>
+                </ul>
             </section>
             <section>
                 <hr className="mb-1" />
-                <p className="text-center text-white">&#169; 2025 Mimon. All rights reserved.</p>
+                <p className="text-center text-white font-poppins-light">&#169; 2025 Mimon. All rights reserved.</p>
             </section>
         </footer>
     )
