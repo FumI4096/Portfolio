d5c0a01482d55d7fd0c90d5d92006295df7f305d
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-01-25 18:54:14 +0800

    Add certification page and certification card component

--- src/components/CertificateCard.tsx
+++ src/components/CertificateCard.tsx
@@ -1,8 +1,24 @@
 interface CardProps{
     title: string
+    picture: string
+    issued: string
 
 }
 
-export default function CertificateCard(){
-    return
+export default function CertificateCard({title, picture, issued} : CardProps){
+    return (
+        <li >
+            <a href="https://www.credly.com/users/seimon-maiquez" target="_blank" rel="noopener" className="h-full group w-full bg-primary-gray-2 p-8 flex flex-col gap-7 justify-between shadow-inset-custom-1 text-white hover:-translate-y-5 transition-all duration-300 cursor-pointer">
+                <div className="h-auto w-full flex gap-10 ">
+                    <img src={picture} alt="" className="h-40 float-left drop-shadow-lg"/>
+                    <div className="flex flex-col justify-center relative">
+                        <p className="font-poppins-semibold break-words text-2xl">{title}</p>
+                        <p className="font-medium">{issued}</p>
+                        <span className="transition-all duration-20 self-end group-hover:block hidden font-poppins-light absolute bottom-0">Click to See More</span>
+                    </div>
+
+                </div>
+            </a>
+        </li>
+    )
 }
--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -1,7 +1,23 @@
+import JavaCert from '../assets/images/java.png';
+import HTML5Cert from '../assets/images/html5.png';
+import DBCert from '../assets/images/database.png';
+import ExcelCert from '../assets/images/excel.png';
+import CertificateCard from '../components/CertificateCard';
+
+
 export default function Certificate(){
     return (
-        <section>
+        <section className='h-1000 flex flex-col bg-primary-gray-1 px-14rem py-10rem items-center justify-between '>
+            <h2 className="font-poppins-semibold text-5xl text-white">My Certificates</h2>
+            <ul className='grid grid-cols-2 h-auto w-full gap-11'>
+                <CertificateCard title='Microsoft Office Specialist: Excel Associate (Office 2019)' picture={ExcelCert} issued='1/17/23'/>
             
+                <CertificateCard title='Information Technology Specialist: Databases' picture={DBCert} issued='1/5/24'/>
+                
+                <CertificateCard title='Information Technology Specialist: HTML5 Application Development' picture={HTML5Cert} issued='6/5/24'/>
+                <CertificateCard title='Information Technology Specialist: Java' picture={JavaCert} issued='1/9/25'/>
+                
+            </ul>
         </section>
     )
 }
