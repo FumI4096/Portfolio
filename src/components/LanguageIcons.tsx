6e25b6563460c217197cff14de14af0920e8d6be
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-01-29 08:57:29 +0800

    Change styling

--- src/components/LanguageIcons.tsx
+++ src/components/LanguageIcons.tsx
@@ -9,7 +9,7 @@ export default function LanguageIcon({icon: Icon, iconLanguage} : IconProps){
     return (
         <div className="">
             <div className="p-6 group bg-primary-gray-2 rounded-md shadow-2xl relative ">
-                <Icon className="text-6xl text-white group-hover:blur-sm group-hover:scale-75 group-hover:text-gray-hover-1 duration-100" />
+                <Icon className="text-6xl text-white group-hover:blur-xs group-hover:scale-75 group-hover:text-gray-hover-1 duration-100" />
                 <div className="hidden absolute justify-center items-center h-full w-full bg-black bg-opacity-25 duration-100 inset-0 group-hover:flex">
                     <span className="text-base text-wrap text-center p-10 font-bold text-white duration-100 font-mono">{iconLanguage}</span>
                 </div>
