30080ae2c951a9094b54a03bcd78f3b620b0ac49
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-05-05 10:17:11 +0800

    feat: Add changes to background and hover

--- src/components/LanguageIcons.tsx
+++ src/components/LanguageIcons.tsx
@@ -10,7 +10,7 @@ export default function LanguageIcon({icon: Icon, iconLanguage} : IconProps){
         <div className="">
             <div className="p-6 group bg-primary-gray-2 rounded-md shadow-2xl relative ">
                 <Icon className="text-6xl text-white group-hover:blur-xs group-hover:scale-75 group-hover:text-gray-hover-1 duration-100" />
-                <div className="hidden absolute justify-center items-center h-full w-full bg-black bg-opacity-25 duration-100 inset-0 group-hover:flex">
+                <div className="hidden absolute justify-center items-center h-full w-full bg-black/30  inset-0 group-hover:flex duration-100">
                     <span className="text-base text-wrap text-center p-10 font-bold text-white duration-100 font-mono">{iconLanguage}</span>
                 </div>
             </div>
