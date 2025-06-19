20313728a67774a801aa431a7a07489bcc9aaa68
Seimon Maiquez <Sesmaiquez@gmail.com>
2025-06-19 11:47:10 +0800

    chores: add design on the navigation bars

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,9 +1,9 @@
 import { FaHome } from "react-icons/fa";
-import { GrProjects } from "react-icons/gr";
-import { GrCertificate } from "react-icons/gr";
+import { GrTasks } from "react-icons/gr";
+import { PiCertificateFill } from "react-icons/pi";
 import { FaTools } from "react-icons/fa";
-import { IoIosContacts } from "react-icons/io";
-import { LuMessageSquareCode } from "react-icons/lu";
+import { IoNewspaper } from "react-icons/io5";
+import { BiSolidPhoneCall } from "react-icons/bi";
 import { Link } from "react-router-dom"
 
 function scrollSection(section: string){
@@ -12,41 +12,57 @@ function scrollSection(section: string){
 
 export default function Nav(){
     return (
-        <nav className='scroll-smooth fixed h-auto w-auto flex flex-col gap-11 bg-white rounded-full p-6 justify-between item-center left-11 top-[50%] -translate-y-[50%]'>
+        <nav className='scroll-smooth fixed h-auto w-auto flex flex-col gap-7 rounded-full justify-between item-center left-11 top-[50%] -translate-y-[50%]'>
             <a
                 onClick={() => scrollSection("/#home")}
-                className={`group`}
+                className="group cursor-pointer relative"
             >
-                <span className="hidden">Home</span>
-                <FaHome className="text-[1.30rem] cursor-pointer"/>
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Home</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <FaHome className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </a>
             <a
                 onClick={() => scrollSection("/#projects")}
+                className="group cursor-pointer relative"
             >
-                <span className="hidden">Projects</span>
-                <GrProjects className="text-[1.30rem] cursor-pointer"/>
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Projects</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <GrTasks className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </a>
             <a
                 onClick={() => scrollSection("/#credentials")}
+                className="group cursor-pointer relative"
             >
-                <span className="hidden">Credentials</span>
-                <GrCertificate className="text-[1.30rem] cursor-pointer"/>
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Credentials</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <PiCertificateFill className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </a>
             <a
                 onClick={() => scrollSection("/#services")}
+                className="group cursor-pointer relative"
             >
-                <span className="hidden">Services</span>
-                <FaTools className="text-[1.30rem] cursor-pointer"/>
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Services</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <FaTools className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </a>
             <a
                 onClick={() => scrollSection("/#contacts")}
+                className="group cursor-pointer relative"
             >
-                <span className="hidden">Contacts</span>
-                <IoIosContacts className="text-[1.30rem] cursor-pointer"/>
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Contacts</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <BiSolidPhoneCall className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </a>
-            <Link to='/blog'>
-                <span className="hidden">Contacts</span>
-                <LuMessageSquareCode className="text-[1.30rem] cursor-pointer"/>
+            <Link to='/blog' className="group cursor-pointer relative">
+                <span className="hidden opacity-0 absolute text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium group-hover:block group-hover:left-3 group-hover:animate-nav-display">Blog</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition group-hover:bg-white">
+                    <IoNewspaper className="text-[1.45rem] text-white transition group-hover:text-primary-gray-2"/>
+                </div>
             </Link>
         </nav>
     )
