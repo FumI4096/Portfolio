132b0fc4871920b1c300678df92c2219aecc1024
Mimon <sesmaiquez@gmail.com>
2026-02-01 10:14:31 +0800

    feat: comment out Blog component code for future use

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -1,24 +1,24 @@
-import { Link } from 'react-router-dom';
-import Icon from '../assets/images/icon.png';
-import { IoMdArrowRoundBack } from "react-icons/io";
+// import { Link } from 'react-router-dom';
+// import Icon from '../assets/images/icon.png';
+// import { IoMdArrowRoundBack } from "react-icons/io";
 
-export default function Blog(){
-    return (
-        <>
-            <header className="bg-primary-gray-2 flex justify-between items-center px-[20rem] py-4 h-auto">
-                <Link to='/' className='flex justify-between items-center w-[90px] px-4 rounded-2xl bg-white font-poppins-medium py-2'>
-                    <IoMdArrowRoundBack />
-                    <p>Back</p>
-                </Link>
-                <img src={Icon} alt="logo" className='h-9'/>
-            </header>
-            <section className="bg-primary-gray-1 h-1000 px-[20rem]">
-                <h1>This is a blog</h1>
-                <code>
+// export default function Blog(){
+//     return (
+//         <>
+//             <header className="bg-primary-gray-2 flex justify-between items-center px-[20rem] py-4 h-auto">
+//                 <Link to='/' className='flex justify-between items-center w-[90px] px-4 rounded-2xl bg-white font-poppins-medium py-2'>
+//                     <IoMdArrowRoundBack />
+//                     <p>Back</p>
+//                 </Link>
+//                 <img src={Icon} alt="logo" className='h-9'/>
+//             </header>
+//             <section className="bg-primary-gray-1 h-1000 px-[20rem]">
+//                 <h1>This is a blog</h1>
+//                 <code>
 
-                </code>
-            </section>
-        </>
+//                 </code>
+//             </section>
+//         </>
         
-    )
-}
+//     )
+// }
