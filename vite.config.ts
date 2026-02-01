589ca4cf9588b6729fb1dedb2e8d9562fc993e67
Mimon <sesmaiquez@gmail.com>
2026-02-01 13:25:10 +0800

    feat: update Vite build configuration to specify output directory and enable directory cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './dist'
+    outDir: 'client/dist', // Output relative to root (which is the client folder)
+    emptyOutDir: true, // Clean the output directory before building
   },
 })
