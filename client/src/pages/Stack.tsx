e221851e2d965862e4ced8761f4b17e503f50195
Mimon <sesmaiquez@gmail.com>
2026-03-19 17:43:29 +0800

    refactor: modify language icons and added new languages and tools: html, css, javascript, openai, redis, and speechgenio

--- client/src/pages/Stack.tsx
+++ client/src/pages/Stack.tsx
@@ -1,16 +1,23 @@
-import { FaJava, FaReact, FaPython, FaAndroid, FaPhp, FaNodeJs, FaDigitalOcean } from "react-icons/fa";
+import { FaJava, FaReact, FaPython, FaAndroid, FaPhp, FaNodeJs, FaDigitalOcean, FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
 import { FaGitAlt } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiTypescript, SiSqlite, SiCanva  } from "react-icons/si";
-import { DiMsqlServer, DiMysql  } from "react-icons/di";
+import { DiMsqlServer, DiMysql, DiRedis  } from "react-icons/di";
+import { AiFillOpenAI } from "react-icons/ai";
 import { BiLogoFlask } from "react-icons/bi";
+import { LuSpeech } from "react-icons/lu";
+
 import LanguageIcon from "../components/LanguageIcons";
 
 export default function Stack(){
     return (
-        <section id="services" className='bg-primary-gray-1 h-auto py-10rem px-14rem flex items-center justify-between flex-col max-2xl:h-auto max-xl:py-6rem max-xl:px-8rem max-md:px-[4.5rem] max-xs:px-[2.5rem]'>
-            <h2 className="font-poppins-semibold text-5xl mb-[15rem] text-white max-2xl:mb-[5rem] max-md:text-4xl">Services &#38; Tools</h2>
-            <article className="h-[400px] flex flex-wrap justify-center items-center gap-20 max-2xl:h-auto">
+        <section id="services" className='bg-primary-gray-1 h-auto py-5 px-14rem flex items-center justify-between flex-row max-xl:py-6rem max-xl:px-8rem max-md:px-[4.5rem] max-xs:px-[2.5rem]'>
+            <h2 className="font-poppins-semibold text-5xl text-white ">Services &#38; Tools</h2>
+            <article className="h-auto grid grid-cols-5 gap-5 max-2xl:h-auto">
+                <LanguageIcon icon={FaHtml5} iconLanguage="HTML" />
+                <LanguageIcon icon={FaCss3Alt} iconLanguage="CSS" />
+                <LanguageIcon icon={IoLogoJavascript} iconLanguage="JavaScript" />
                 <LanguageIcon icon={FaJava} iconLanguage="Java" />
                 <LanguageIcon icon={FaReact} iconLanguage="ReactJS"/>
                 <LanguageIcon icon={FaNodeJs} iconLanguage="NodeJS"/>
@@ -23,9 +30,12 @@ export default function Stack(){
                 <LanguageIcon icon={DiMsqlServer} iconLanguage="MS SQL Server"/>
                 <LanguageIcon icon={DiMysql} iconLanguage="MySQL"/>
                 <LanguageIcon icon={SiSqlite} iconLanguage="SQLite"/>
+                <LanguageIcon icon={DiRedis} iconLanguage="Redis"/>
                 <LanguageIcon icon={SiCanva} iconLanguage="Canva"/>
                 <LanguageIcon icon={FaGitAlt} iconLanguage="Git"/>
                 <LanguageIcon icon={FaDigitalOcean} iconLanguage="Digital Ocean"/>
+                <LanguageIcon icon={AiFillOpenAI} iconLanguage="OpenAI"/>
+                <LanguageIcon icon={LuSpeech} iconLanguage="Speechgen.io"/>                
             </article>
         </section>
     )
