fe5c4e8593a6fe88639927e76e3761ac613a9e01
Seimon Elias Maiquez <123810885+FumI4096@users.noreply.github.com>
2026-04-08 16:12:42 +0800

    feat: add experience tab

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,7 +1,6 @@
 import { FaHome } from "react-icons/fa";
 import { GrTasks } from "react-icons/gr";
 import { PiCertificateFill } from "react-icons/pi";
-import { FaTools } from "react-icons/fa";
 import { BiSolidPhoneCall } from "react-icons/bi";
 import { BsPersonFillGear } from "react-icons/bs";
 function scrollSection(section: string){
@@ -36,21 +35,21 @@ export default function Nav(){
                 </div>
             </a>
             <a
-                onClick={() => scrollSection("/#experience")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Experiences</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
-                    <BsPersonFillGear className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
-            <a
                 onClick={() => scrollSection("/#projects")}
                 className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
             >
                 <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Projects</span>
                 <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
                     <GrTasks className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
+                </div>
+            </a>
+            <a
+                onClick={() => scrollSection("/#experience")}
+                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
+            >
+                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Experiences</span>
+                <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
+                    <BsPersonFillGear className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
                 </div>
             </a>
             <a
@@ -62,15 +61,6 @@ export default function Nav(){
                     <PiCertificateFill className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
                 </div>
             </a>
-            {/* <a
-                onClick={() => scrollSection("/#services")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Services</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition lg:group-hover:bg-white max-lg:border-none">
-                    <FaTools className="text-[1.45rem] text-white transition lg:group-hover:text-primary-gray-2"/>
-                </div>
-            </a> */}
             <a
                 onClick={() => scrollSection("/#contacts")}
                 className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
