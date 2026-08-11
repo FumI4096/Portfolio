c87ac56523215e594454524c3d82205ac6347543
FumI4096 <sesmaiquez@gmail.com>
2026-08-11 14:01:10 +0800

    chore: optimize navigation bar icons and responsiveness

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -3,11 +3,21 @@ import { GrTasks } from "react-icons/gr";
 import { PiCertificateFill } from "react-icons/pi";
 import { BiSolidPhoneCall } from "react-icons/bi";
 import { BsPersonFillGear } from "react-icons/bs";
+
 function scrollSection(section: string){
     window.location.href = section
 }
 
 export default function Nav(){
+
+    const navItems = [
+        { icon: FaHome, label: "Home", section: "/#home" },
+        { icon: GrTasks, label: "Projects", section: "/#projects" },
+        { icon: BsPersonFillGear, label: "Experiences", section: "/#experience" },
+        { icon: PiCertificateFill, label: "Credentials", section: "/#credentials" },
+        { icon: BiSolidPhoneCall, label: "Contacts", section: "/#contacts" }
+    ]
+
     return (
         <nav className='fixed h-auto w-auto flex flex-col gap-7 rounded-full justify-between item-center left-11 top-[50%] -translate-y-[50%] z-50 max-xl:left-7 
         max-lg:w-full 
@@ -25,51 +35,20 @@ export default function Nav(){
         max-xs:px-2
         '
         >
-            <a
-                onClick={() => scrollSection("/#home")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Home</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition lg:group-hover:bg-white max-lg:border-none">
-                    <FaHome className="text-[1.45rem] text-white transition lg:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
-            <a
-                onClick={() => scrollSection("/#projects")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Projects</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
-                    <GrTasks className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
-            <a
-                onClick={() => scrollSection("/#experience")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Experiences</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
-                    <BsPersonFillGear className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
-            <a
-                onClick={() => scrollSection("/#credentials")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Credentials</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition xl:group-hover:bg-white max-lg:border-none">
-                    <PiCertificateFill className="text-[1.45rem] text-white transition xl:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
-            <a
-                onClick={() => scrollSection("/#contacts")}
-                className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
-            >
-                <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.60rem]">Contacts</span>
-                <div className="p-3 rounded-full border-3 flex justify-center items-center transition lg:group-hover:bg-white max-lg:border-none">
-                    <BiSolidPhoneCall className="text-[1.45rem] text-white transition lg:group-hover:text-primary-gray-2"/>
-                </div>
-            </a>
+            {navItems.map((item, index) => (
+                <a
+                    key={index}
+                    onClick={() => scrollSection(item.section)}
+                    className="group cursor-pointer relative max-lg:flex max-lg:flex-col-reverse max-lg:items-center max-lg:gap-1.5"
+                >
+                    <span className="hidden opacity-0 absolute left-3 text-0 px-11 top-[50%] -translate-y-[50%] text-white font-poppins-medium lg:group-hover:block lg:group-hover:animate-nav-display max-lg:static max-lg:block max-lg:opacity-100 max-lg:p-0 max-lg:text-[0.80rem] max-md:text-[0.70rem] max-sm:text-[0.44rem]">
+                        {item.label}
+                    </span>
+                    <div className="p-3 rounded-full border-3 flex justify-center items-center transition lg:group-hover:bg-white max-lg:border-none">
+                        <item.icon className="text-[1.25rem] text-white transition lg:group-hover:text-primary-gray-2 md-text-[1.45rem]"/>
+                    </div>
+                </a>
+            ))}
         </nav>
     )
 }
