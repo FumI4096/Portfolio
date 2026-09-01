9a319d2dba21ff56c1b3a38b6fe5895f00daab79
FumI4096 <sesmaiquez@gmail.com>
2026-09-01 15:03:05 +0800

    chore: change position of the title header and client header

--- client/src/components/ExperienceCard.tsx
+++ client/src/components/ExperienceCard.tsx
@@ -5,13 +5,13 @@ export default function ExperienceCard({ date, role, title, client, orderedList,
         <aside className="bg-primary-gray-2 flex items-center justify-between w-full p-[6rem] rounded-xl max-lg:p-[4rem] max-sm:p-[3rem]">
             <article className="w-full flex flex-col">
                 <span className="text-white text-[2.20rem] font-poppins-semibold w-full max-sm:text-[1.70rem] max-sm:text-center">{date}</span>
-                <hr className="my-7 max-sm:my-2" />
                 <span className="text-white text-[1.30rem] font-poppins-medium w-full max-sm:text-center max-sm:text-[1rem] max-sm:mb-2">
                     {role} {title ? `• ${title}` : ''}
                 </span>
-                <span className="text-sm text-gray-300 font-poppins-light mb-4 w-full max-sm:text-[0.9rem] max-sm:text-center">
+                <span className="text-sm text-gray-300 font-poppins-light w-full max-sm:text-[0.9rem] max-sm:text-center">
                     {client}
                 </span>
+                <hr className="my-7 max-sm:my-2" />
                 <ul className="text-white list-disc pl-5 font-poppins-light text-[0.90rem] w-full mb-10">
                     {orderedList.map((item: string, index: number) => (
                         <li key={index}>{item}</li>
